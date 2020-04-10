c990ddec05bcce7d078f35faf9a3b4c62b1deb4a
Zijian Zhou <zzhou3@ualberta.ca>
2020-04-10 15:52:11 -0600

    add color

---
+++ frontend/covid-19-tracker/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+
+    <div className="App_header">
+      <h1>Covid-19 Tendency in Alberta</h1>
+    </div>
+    </body>
+  );
+}
+
+export default App;
