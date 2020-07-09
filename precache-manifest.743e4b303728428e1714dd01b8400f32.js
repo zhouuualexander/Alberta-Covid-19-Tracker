4e8394805a62b72f2266244803be5b7658b84c9b
zijian zhou <alexzhou@AlexZhous-MBP.hitronhub.home>
2020-07-08 23:24:32 -0600

    Updates

---
+++ precache-manifest.743e4b303728428e1714dd01b8400f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607ae5bfe5af0f2b7c85b81b81ea5b5d",
+    "url": "/alberta-covid-19-tracker/index.html"
+  },
+  {
+    "revision": "aaaf2939e616c58ddcb9",
+    "url": "/alberta-covid-19-tracker/static/css/main.b77d3866.chunk.css"
+  },
+  {
+    "revision": "ce2c25b12a63b0cd2edf",
+    "url": "/alberta-covid-19-tracker/static/js/2.f8957149.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/alberta-covid-19-tracker/static/js/2.f8957149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaaf2939e616c58ddcb9",
+    "url": "/alberta-covid-19-tracker/static/js/main.0f9c4f00.chunk.js"
+  },
+  {
+    "revision": "1f20b713a2693adbf600",
+    "url": "/alberta-covid-19-tracker/static/js/runtime-main.a7e4f3ce.js"
+  }
+]);
